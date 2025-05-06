f50e5bfc85e449288ece4f4226b30dc812c20c8c
Jiras403 <jiri.siladi@infis.cz>
2025-05-06 11:31:32 +0200

    Add files via upload

---
+++ siladi.py
@@ -0,0 +1,7 @@
+import random
+
+
+pole = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+v4 = random.choice(pole)
+index = pole.index(v4)
+print("Vybral jsem písmeno " + v4 + " na indexu " + str(index))
