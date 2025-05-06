f3c17906d57701eef333a73702bdfa3380506e3b
DimaLOp28 <sofiia.nehreba@infis.cz>
2025-05-06 11:31:33 +0200

    Update heslo.py

--- heslo.py
+++ heslo.py
@@ -1,5 +1,5 @@
+import random
 jidlo =["pizza", "špagety", "hamburger", "sushi", "řízek"]
-
 v8 = random.choice(jidlo)
-
-    print(v8)
+index = jidlo.index(v8)
+print("Vybral jsem jídlo " + v8 + " co se vybralo " + "na indexu "+ index)
