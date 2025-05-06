3d688780359169714c1b6c0ed23f4b61eb483329
lanyitereza <tereza.lanyi@infis.cz>
2025-05-06 11:30:42 +0200

    heslo.py

---
+++ heslo.py
@@ -0,0 +1,8 @@
+import random
+slova = ["cat, sun, pink, red, blue"]
+
+v11 = random.choice(slova)
+
+pozice = slova.index(v11)
+
+print("Vybral jsem anglické slovo " + v11 + " co se vybralo " + "na indexu " + str(index))
