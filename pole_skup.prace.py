fee08356ad4a8a55b70def4f4f93c36f30ac288c
Nutys73 <natalie.stefkova@infis.cz>
2025-05-06 11:32:41 +0200

    Create pole_skup.prace.py

---
+++ pole_skup.prace.py
@@ -0,0 +1,5 @@
+import random
+skolniPredmety=["matematika", "literatura", "biologie", "hardware", "tělocvik"]
+v10 = random.choice(skolniPredmety)
+pozice = skolniPredmety.index(v10)
+print("Vybral jsem předmět", v10, "co se vybral,na indexu", pozice )
